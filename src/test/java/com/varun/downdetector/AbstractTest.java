3964f76bee4aae19410ead36a65ec76be40dcf12
varun <varunkumar.kunchakuri@gmail.com>
2020-12-26 20:01:43 +0530

    Running Unit tests

---
+++ src/test/java/com/varun/downdetector/AbstractTest.java
@@ -0,0 +1,20 @@
+package com.varun.downdetector;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest(classes = DowndetectorApplication.class)
+@WebAppConfiguration
+public abstract class AbstractTest {
+   protected MockMvc mvc;
+   @Autowired
+   WebApplicationContext webApplicationContext;
+
+   protected void setUp() {
+      mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+   }
+}
---
+++ src/test/java/com/varun/downdetector/controllers/UrlCheckControllerTests.java
@@ -0,0 +1,34 @@
+package com.varun.downdetector.controllers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.varun.downdetector.AbstractTest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+public class UrlCheckControllerTests extends AbstractTest {
+
+    @Override
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = { "https://www.google.com,Site is up!", "https://www.varun.com,Site is down!"})
+    public void getUrlStatusMessage(String url, String result) throws Exception {
+        String uri = "/check?url=" + url;
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri).accept(MediaType.APPLICATION_JSON_VALUE))
+                .andReturn();
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        assertEquals(result, content);
+    }
+
+}

4c50ceaf5c05c7205cb8be09171882364ad22281
varun <varunkumar.kunchakuri@gmail.com>
2020-12-26 20:19:22 +0530

    Added All the 3 cases and null case

--- src/test/java/com/varun/downdetector/controllers/UrlCheckControllerTests.java
+++ src/test/java/com/varun/downdetector/controllers/UrlCheckControllerTests.java
@@ -7,10 +7,13 @@ import com.varun.downdetector.AbstractTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+@RunWith(JUnitPlatform.class)
 public class UrlCheckControllerTests extends AbstractTest {
 
     @Override
@@ -20,7 +23,10 @@ public class UrlCheckControllerTests extends AbstractTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = { "https://www.google.com,Site is up!", "https://www.varun.com,Site is down!"})
+    @CsvSource(value = { "https://www.google.com,Site is up!", 
+    "https://www.varun.com,Site is down!",
+    "randomstring,Not a valid URL",
+",Not a valid URL"})
     public void getUrlStatusMessage(String url, String result) throws Exception {
         String uri = "/check?url=" + url;
         MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri).accept(MediaType.APPLICATION_JSON_VALUE))

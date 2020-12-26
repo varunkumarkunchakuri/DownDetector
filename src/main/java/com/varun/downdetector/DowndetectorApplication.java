c45880c1a2004f0fe7b47a487b581903838f1548
varun <varunkumar.kunchakuri@gmail.com>
2020-12-26 10:48:10 +0530

    first commit

---
+++ src/main/java/com/varun/downdetector/DowndetectorApplication.java
@@ -0,0 +1,13 @@
+package com.varun.downdetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DowndetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DowndetectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/varun/downdetector/DowndetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varun.downdetector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DowndetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

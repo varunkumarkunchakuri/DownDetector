0703b760dc46c9e6c60cb61e0cf0f6f8e3e82b5b
varun <varunkumar.kunchakuri@gmail.com>
2020-12-26 11:22:40 +0530

    Added a controller which checks if the website is up

---
+++ src/main/java/com/varun/downdetector/controllers/UrlCheckController.java
@@ -0,0 +1,43 @@
+package com.varun.downdetector.controllers;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UrlCheckController {
+
+    private final String SITE_IS_UP = "Site is up!";
+
+    private final String SITE_IS_DOWN = "Site is down!";
+
+    private final String INVALID_URL = "Not a valid URL";
+
+    @GetMapping("/check")
+    public String getUrlStatusMessage(@RequestParam String url) {
+        String returnMessage = "";
+        try {
+            URL urlObj = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) urlObj.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            if(connection.getResponseCode()>=400){
+                returnMessage = SITE_IS_DOWN;
+            }else{
+                returnMessage = SITE_IS_UP;
+            }
+        } catch (MalformedURLException e) {
+            returnMessage = INVALID_URL;
+        } catch (IOException e) {
+            returnMessage = SITE_IS_DOWN;
+        }
+
+
+        return returnMessage;
+    }
+}
